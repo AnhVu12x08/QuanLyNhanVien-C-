211233164d6d9359261d14b4d11fc95dd6e1f866
anhvu12x08 <truonganhvu120804@gmail.com>
2023-09-10 23:33:45 +0700

    Add project files.

---
+++ QLNV/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLNV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nhanvien nv1 = new Nhanvien();
+            Nhanvien nv2 = new Nhanvien("NV02", "Nguyen Van B", 2.5f, 2010);
+            Nhanvien nv3 = new Nhanvien("NV03", "Nguyen Van C", 3.5f, 2011);
+            nv1.Xuat();
+        }
+    }
+}
---
+++ QLNV/QLNhanVien.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace QLNV
+{
+    class Nhanvien
+    {
+        private string ms;
+        private string ht;
+        private float hsl;
+        private int nvl;
+        public static int MLTT = 1750000;
+
+        public string MaSo 
+        {
+            get 
+            { 
+                return ms; 
+            }
+            set 
+            { 
+                ms = value; 
+            }
+        }
+        public string HoTen
+        {
+            get 
+            { 
+                return ht; 
+            }
+            set 
+            { 
+                ht = value; 
+            }
+        }
+        public float HeSoLuong
+        {
+            get 
+            { 
+                return hsl; 
+            }
+            set 
+            {
+                if (value <0)
+                    hsl = 0;
+                else
+                hsl = value; 
+            }
+        }
+        public int NamVaoLam
+        {
+            get 
+            { 
+                return nvl; 
+            }
+            set
+            {
+                if (value < 0)
+                    nvl = 0;
+                else
+                nvl = value; 
+            }
+        }
+        public Nhanvien()
+        {
+            ms = "";
+            ht = "";
+            hsl = 0;
+            nvl = 0;
+        }
+        public Nhanvien (string ms, string ht, float hsl, int nvl)
+        {
+            MaSo = ms;
+            HoTen = ht;
+            HeSoLuong = hsl;
+            NamVaoLam = nvl;
+        }
+        float LuongCoBan()
+        {
+            return HeSoLuong * MLTT;
+        }
+        public float HeSoPhuCapTN()
+        {
+            return (DateTime.Today.Year - NamVaoLam) / 100.0f;
+        }
+        public float Luong()
+        {
+            return LuongCoBan()*HeSoPhuCapTN();
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap vao ma so cho nhan vien");
+            MaSo = Console.ReadLine();
+            Console.WriteLine("Nhap vao ho ten cho nhan vien");
+            HoTen = Console.ReadLine();
+            Console.WriteLine("Nhap vao he so luong cho nhan vien");
+            HeSoLuong = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao nam vao lam cho nhan vien");
+            NamVaoLam = int.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Ma so: {0}", MaSo);
+            Console.WriteLine("Ho ten: {0}", HoTen);
+            Console.WriteLine("He so luong: {0}", HeSoLuong);
+            Console.WriteLine("Nam vao lam: {0}", NamVaoLam);
+            Console.WriteLine("Luong co ban: {0}", LuongCoBan());
+            Console.WriteLine("He so phu cap tham nien: {0}", HeSoPhuCapTN());
+            Console.WriteLine("Luong: {0}", Luong());
+        }
+    }
+}
